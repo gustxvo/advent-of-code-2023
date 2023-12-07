f4c92ef9294619c5b1ba7f27a774ef9711df31b9
gustxvo <gualmeida2004@gmail.com>
2023-12-07 17:34:05 -0300

    Solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
+
+    fun String.replaceDigits() =
+        replace("one", "o1e")
+            .replace("two", "t2o")
+            .replace("three", "t3e")
+            .replace("four", "f4r")
+            .replace("five", "f5e")
+            .replace("six", "s6x")
+            .replace("seven", "s7n")
+            .replace("eight", "e8h")
+            .replace("nine", "n9e")
+
+    fun String.firstAndLastDigits(): Int {
+        val first = first { it.isDigit() }
+        val last = last { it.isDigit() }
+        return ("$first$last").toInt()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { it.firstAndLastDigits() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            line.replaceDigits().run { firstAndLastDigits() }
+        }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+
 }
